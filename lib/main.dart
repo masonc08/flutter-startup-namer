f917828c5a389ba128eccfc3e77d9ff22417afa5
Mason Cao <32713681+masonc08@users.noreply.github.com>
2019-09-07 17:43:46 -0400

    implemented random verbs

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -25,46 +28,46 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  Future<String> loadHTML(url) async {
-    final response =  await http.post(url);
+  Future<String> _loadHTML(url) async {
+    http.Response response = await http.post(url);
+//    return jsonDecode(response.body)['data'];
     return response.body;
   }
   final _url = "https://www.randomlists.com/data/verbs.json";
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>[]; //array of WordPairs the user saved
   final _fontSize = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context){
-    printVerbs(url){
-
-    }
     return Scaffold(
       appBar: AppBar(
         title: Center(
-          child: Text('Startup Name Generator'),
+          child: Text('Church'),
         ),
       ),
-      body: _buildSuggestions(),
+      body: FutureBuilder(
+        future: _loadHTML(_url),
+        builder: (context, body){
+          var verbs = jsonDecode(body.data.toString())['data'];
+          return _buildSuggestions(verbs);
+        }
+      )
     );
   }
-  Widget _buildSuggestions() {
+  Widget _buildSuggestions(body) {
     return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
         if (i.isOdd) return Divider();
         final index = i~/2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index], index);
+        Random rand = new Random();
+        return _buildRow(index, body[rand.nextInt(body.length)]);
       }
     );
   }
-  Widget _buildRow(WordPair word, int index){
-    final alreadySaved = _saved.contains(word);
+  Widget _buildRow(index, verb){
+    String displayText = '${index + 1}. succ $verb sydney';
     return ListTile(
       title: Text(
-        (index + 1).toString() + '. ' + word.asPascalCase,
+        displayText,
         style: _fontSize,
       ),
     );
